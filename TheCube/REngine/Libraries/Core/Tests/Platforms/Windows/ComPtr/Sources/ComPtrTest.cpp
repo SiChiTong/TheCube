9dae8ec514c1448c311c07715eea4738626e6231
Zingam <zingam@outlook.com>
2017-12-21 11:17:54 +0200

    ComPtr: Added some free functions and logic operators

--- TheCube/REngine/Libraries/Core/Tests/Platforms/Windows/ComPtr/Sources/ComPtrTest.cpp
+++ TheCube/REngine/Libraries/Core/Tests/Platforms/Windows/ComPtr/Sources/ComPtrTest.cpp
@@ -18,106 +18,9 @@
 // Test Suits
 ////////////////////////////////////////////////////////////////////////////////
 
-TEST (ComPtr, DefaultConstructor)
-{
-  REngine::Core::Windows::ComPtr<IBase> base;
-  ASSERT_EQ (nullptr, base.Get ());
-  // Test address of internal pointer
-  ASSERT_NE (nullptr, base.GetAddressOf ());
-  ASSERT_NE (nullptr, &base);
-}
-
-TEST (ComPtr, CopyConstructor)
-{
-  REngine::Core::Windows::ComPtr<IBase> base1;
-  ASSERT_EQ (nullptr, base1.Get ());
-  CreateDerivedObject (&base1);
-  ASSERT_TRUE (base1);
-
-  REngine::Core::Windows::ComPtr<IBase> base2 (base1);
-  ASSERT_TRUE (base2);
-  ASSERT_EQ (base1.Get (), base2.Get ());
-
-  REngine::Core::Windows::ComPtr<IUnknown> unknown (base1);
-  ASSERT_TRUE (unknown);
-  ASSERT_EQ (unknown.Get (), base2.Get ());
-}
-
-TEST (ComPtr, CopyAssignment)
-{
-  REngine::Core::Windows::ComPtr<IBase> base1;
-  ASSERT_EQ (nullptr, base1.Get ());
-  CreateDerivedObject (&base1);
-  ASSERT_TRUE (base1);
-
-  REngine::Core::Windows::ComPtr<IBase> base2;
-  ASSERT_EQ (nullptr, base2.Get ());
-
-  base2 = base1;
-  ASSERT_TRUE (base2);
-  ASSERT_EQ (base1.Get (), base2.Get ());
-
-  REngine::Core::Windows::ComPtr<IUnknown> unknown;
-  ASSERT_EQ (nullptr, unknown.Get ());
-  unknown = base1;
-  ASSERT_TRUE (unknown);
-  ASSERT_EQ (unknown.Get (), base2.Get ());
-}
-
-TEST (ComPtr, MoveConstructor)
-{
-  REngine::Core::Windows::ComPtr<IBase> base1;
-  ASSERT_EQ (nullptr, base1.Get ());
-  CreateDerivedObject (&base1);
-  ASSERT_TRUE (base1);
-
-  auto basePtr = base1.Get ();
-  basePtr->AddRef ();
-
-  REngine::Core::Windows::ComPtr<IBase> base2 (std::move (base1));
-  ASSERT_TRUE (base2);
-  ASSERT_FALSE (base1);
-  ASSERT_EQ (basePtr, base2.Get ());
-
-  REngine::Core::Windows::ComPtr<IUnknown> unknown (std::move (base2));
-  ASSERT_TRUE (unknown);
-  ASSERT_FALSE (base2);
-  ASSERT_EQ (basePtr, unknown.Get ());
-
-  basePtr->Release ();
-}
-
-TEST (ComPtr, MoveAssignment)
-{
-  REngine::Core::Windows::ComPtr<IBase> base1;
-  ASSERT_EQ (nullptr, base1.Get ());
-  CreateDerivedObject (&base1);
-  ASSERT_TRUE (base1);
-
-  auto basePtr = base1.Get ();
-  basePtr->AddRef ();
-
-  REngine::Core::Windows::ComPtr<IBase> base2;
-  ASSERT_EQ (nullptr, base2.Get ());
-
-  base2 = std::move (base1);
-  ASSERT_TRUE (base2);
-  ASSERT_FALSE (base1);
-  ASSERT_EQ (basePtr, base2.Get ());
-
-  REngine::Core::Windows::ComPtr<IUnknown> unknown;
-  ASSERT_EQ (nullptr, unknown.Get ());
-  unknown = std::move (base2);
-  ASSERT_TRUE (unknown);
-  ASSERT_FALSE (base2);
-  ASSERT_EQ (basePtr, unknown.Get ());
-
-  basePtr->Release ();
-}
-
 TEST (ComPtr, ComPtr)
 {
-  REngine::Core::Windows::ComPtr<IBase> base;
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base;
   ASSERT_NE (nullptr, &base);
   switch (auto result = CreateDerivedObject (&base); result)
   {
@@ -148,7 +51,7 @@ TEST (ComPtr, ComPtr)
     }
   }
 
-  REngine::Core::Windows::ComPtr<IFeature> feature;
+  REngine::Core::Platforms::Windows::ComPtr<IFeature> feature;
   switch (auto result = base->QueryInterface<IFeature> (&feature); result)
   {
     case S_OK:
@@ -178,7 +81,7 @@ TEST (ComPtr, ComPtr)
     }
   }
 
-  REngine::Core::Windows::ComPtr<IDerived> derived;
+  REngine::Core::Platforms::Windows::ComPtr<IDerived> derived;
   ASSERT_NE (nullptr, &derived);
   switch (auto result = base->QueryInterface<IDerived> (&derived); result)
   {
@@ -210,9 +113,147 @@ TEST (ComPtr, ComPtr)
   }
 }
 
+TEST (ComPtr, CopyAssignment)
+{
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base1;
+  ASSERT_EQ (nullptr, base1.Get ());
+  CreateDerivedObject (&base1);
+  ASSERT_TRUE (base1);
+
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base2;
+  ASSERT_EQ (nullptr, base2.Get ());
+
+  base2 = base1;
+  ASSERT_TRUE (base2);
+  ASSERT_EQ (base1.Get (), base2.Get ());
+
+  REngine::Core::Platforms::Windows::ComPtr<IUnknown> unknown;
+  ASSERT_EQ (nullptr, unknown.Get ());
+  unknown = base1;
+  ASSERT_TRUE (unknown);
+  ASSERT_EQ (unknown.Get (), base2.Get ());
+}
+
+TEST (ComPtr, CopyConstructor)
+{
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base1;
+  ASSERT_EQ (nullptr, base1.Get ());
+  CreateDerivedObject (&base1);
+  ASSERT_TRUE (base1);
+
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base2 (base1);
+  ASSERT_TRUE (base2);
+  ASSERT_EQ (base1.Get (), base2.Get ());
+
+  REngine::Core::Platforms::Windows::ComPtr<IUnknown> unknown (base1);
+  ASSERT_TRUE (unknown);
+  ASSERT_EQ (unknown.Get (), base2.Get ());
+}
+
+TEST (ComPtr, DefaultConstructor)
+{
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base;
+  ASSERT_EQ (nullptr, base.Get ());
+  // Test address of internal pointer
+  ASSERT_NE (nullptr, base.GetAddressOf ());
+  ASSERT_NE (nullptr, &base);
+}
+
+TEST (ComPtr, MoveAssignment)
+{
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base1;
+  ASSERT_EQ (nullptr, base1.Get ());
+  CreateDerivedObject (&base1);
+  ASSERT_TRUE (base1);
+
+  auto basePtr = base1.Get ();
+  basePtr->AddRef ();
+
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base2;
+  ASSERT_EQ (nullptr, base2.Get ());
+
+  base2 = std::move (base1);
+  ASSERT_TRUE (base2);
+  ASSERT_FALSE (base1);
+  ASSERT_EQ (basePtr, base2.Get ());
+
+  REngine::Core::Platforms::Windows::ComPtr<IUnknown> unknown;
+  ASSERT_EQ (nullptr, unknown.Get ());
+  unknown = std::move (base2);
+  ASSERT_TRUE (unknown);
+  ASSERT_FALSE (base2);
+  ASSERT_EQ (basePtr, unknown.Get ());
+
+  // Finalize and clean up
+  REngine::Core::Platforms::Windows::ReleaseComInterface (basePtr);
+}
+
+TEST (ComPtr, MoveConstructor)
+{
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base1;
+  ASSERT_EQ (nullptr, base1.Get ());
+  CreateDerivedObject (&base1);
+  ASSERT_TRUE (base1);
+
+  auto basePtr = base1.Get ();
+  basePtr->AddRef ();
+
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base2 (std::move (base1));
+  ASSERT_TRUE (base2);
+  ASSERT_FALSE (base1);
+  ASSERT_EQ (basePtr, base2.Get ());
+
+  REngine::Core::Platforms::Windows::ComPtr<IUnknown> unknown (
+    std::move (base2));
+  ASSERT_TRUE (unknown);
+  ASSERT_FALSE (base2);
+  ASSERT_EQ (basePtr, unknown.Get ());
+
+  // Finalize and clean up
+  REngine::Core::Platforms::Windows::ReleaseComInterface (basePtr);
+}
+
+TEST (ComPtr, FreeFunctions)
+{
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base1;
+  ASSERT_EQ (nullptr, base1.Get ());
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base2;
+  ASSERT_EQ (nullptr, base2.Get ());
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base3;
+  ASSERT_EQ (nullptr, base3.Get ());
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base4;
+  ASSERT_EQ (nullptr, base3.Get ());
+
+  // operator== ()
+  ASSERT_TRUE ((base1 == base2) && (base1 == base3));
+
+  CreateDerivedObject (&base1);
+  ASSERT_TRUE (base1);
+  CreateDerivedObject (&base2);
+  ASSERT_TRUE (base2);
+  base3 = base1;
+
+  // operator== ()
+  ASSERT_FALSE (base1 == base2);
+  ASSERT_TRUE (base1 == base3);
+  ASSERT_FALSE (base1 == base4);
+  CreateDerivedObject (&base4);
+  ASSERT_FALSE (base1 == base4);
+
+  // operator!= ()
+  ASSERT_TRUE (base1 != base2);
+  ASSERT_FALSE (base1 != base3);
+  ASSERT_TRUE (base1 != base4);
+
+  // swap ()
+  swap (base1, base2);
+  ASSERT_TRUE (base2 == base3);
+  ASSERT_TRUE (base1 != base3);
+}
+
 TEST (ComPtr, Methods)
 {
-  REngine::Core::Windows::ComPtr<IBase> base1;
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base1;
   ASSERT_EQ (nullptr, base1.Get ());
   // Test address of internal pointer
   ASSERT_NE (nullptr, base1.GetAddressOf ());
@@ -230,8 +271,7 @@ TEST (ComPtr, Methods)
   ptr->AddRef ();
   ASSERT_EQ (base1Ptr, ptr);
   ASSERT_FALSE (base1);
-  ptr->Release ();
-  ptr = nullptr;
+  REngine::Core::Platforms::Windows::ReleaseComInterface<IBase> (ptr);
 
   // Reset ();
   CreateDerivedObject (&base1);
@@ -242,7 +282,7 @@ TEST (ComPtr, Methods)
   ASSERT_EQ (base1Ptr, base1.Get ());
 
   // Swap ();
-  REngine::Core::Windows::ComPtr<IBase> base2;
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base2;
   ASSERT_EQ (nullptr, base2.Get ());
 
   CreateDerivedObject (&base2);
@@ -256,18 +296,16 @@ TEST (ComPtr, Methods)
   ASSERT_EQ (base2Ptr, base1.Get ());
   ASSERT_EQ (base1Ptr, base2.Get ());
 
-  base2Ptr->Release ();
-  base2Ptr = nullptr;
+  REngine::Core::Platforms::Windows::ReleaseComInterface (base2Ptr);
 
-  // Finalize
-  base1Ptr->Release ();
-  base1Ptr = nullptr;
+  // Finalize and clean up
+  REngine::Core::Platforms::Windows::ReleaseComInterface (base1Ptr);
 }
 
 TEST (ComPtr, Methods_GetAs)
 {
   // GetAs ();
-  REngine::Core::Windows::ComPtr<IBase> base;
+  REngine::Core::Platforms::Windows::ComPtr<IBase> base;
   ASSERT_EQ (nullptr, base.Get ());
   // Test address of internal pointer
   ASSERT_NE (nullptr, base.GetAddressOf ());
@@ -277,7 +315,7 @@ TEST (ComPtr, Methods_GetAs)
   CreateDerivedObject (&base);
   ASSERT_TRUE (base);
 
-  REngine::Core::Windows::ComPtr<IFeature> feature;
+  REngine::Core::Platforms::Windows::ComPtr<IFeature> feature;
   switch (auto result = base.GetAs (feature); result)
   {
     case S_OK:
