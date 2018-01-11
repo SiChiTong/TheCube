7298e89306a2931035aa3f5c3f87216be640ac33
Zingam <zingam@outlook.com>
2018-01-11 16:13:50 +0200

    CMake: Added comments to the Google Test script

--- TheCube/REngine/cmake/REngine_GoogleTest.cmake
+++ TheCube/REngine/cmake/REngine_GoogleTest.cmake
@@ -2,19 +2,21 @@
 include_guard() # Prevent this file from being included more than once
 ################################################################################
 
+# Google Test will be downloaded at the CMake configuration step
+
 option (option_EnableUnitTests "Enable Google Test based unit tests" NO)
 
 if (option_EnableUnitTests)
   set (GoogleTest_Project "${CMAKE_CURRENT_SOURCE_DIR}/Tests")
   set (GoogleTest_Directory "${CMAKE_BINARY_DIR}/GoogleTest")
 
-  # Copy CMakeLists.txt defining the GTest target to the target directory
+  # Copy CMakeLists.txt defining the Google Test target to the target directory
   configure_file (
     "${GoogleTest_Project}/CMakeLists.txt.in"
     "${GoogleTest_Directory}/CMakeLists.txt"
   )
-  # Execute CMake to download GTest and to generate the respective build system
-  # in the target directory
+  # Execute CMake to download Google Test and to generate the respective build
+  # system in the target directory
   execute_process (
     COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
     RESULT_VARIABLE result
@@ -23,7 +25,7 @@ if (option_EnableUnitTests)
   if (result)
     message (FATAL_ERROR "CMake step for googletest failed: ${result}")
   endif()
-  # Build GTest in the target directory
+  # Build Google Test in the target directory
   execute_process (
     COMMAND ${CMAKE_COMMAND} --build .
     RESULT_VARIABLE result
@@ -37,8 +39,8 @@ if (option_EnableUnitTests)
   # settings on Windows
   set (gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-  # Add googletest directly to our build. This defines
-  # the gtest and gtest_main targets.
+  # Add Google Test directly to the build. This defines
+  # the 'gtest' and 'gtest_main' targets.
   add_subdirectory (
     "${GoogleTest_Directory}/GoogleTest/googletest-source"
     "${GoogleTest_Directory}/GoogleTest/googletest-binary"
@@ -49,7 +51,7 @@ if (option_EnableUnitTests)
     PROPERTIES FOLDER
       "${ProjectFolder_Tests}/GoogleTest"
   )
-  # Do not use GTest TR1 tuple
+  # Do not use Google Test TR1 tuple
   target_compile_definitions (gtest
     PRIVATE
       "GTEST_HAS_TR1_TUPLE=0"
